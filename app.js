df28dbcf56139cc905d131288b8ffade45b94b56
kimoabser <karimmosamaa22@gmail.com>
2024-09-24 12:51:41 -0400

    f10

--- app.js
+++ app.js
@@ -30,12 +30,12 @@ const Booking = require('./model/Booking');
 
 // Synchronize model with the database
 User.sync({ force: true }).then(() => console.log('User model synchronized.'));
-Bank.sync({ force: true }).then(() => console.log('Bank model synchronized.'));
-Card.sync({ force: true }).then(() => console.log('Card model synchronized.'));
-CardType.sync({ force: true }).then(() => console.log('CardType model synchronized.'));
-AddMethod.sync({ force: true }).then(() => console.log('AddMethod model synchronized.'));
 Payment.sync({ force: true }).then(() => console.log('Payment model synchronized.'));
 PaymentMethod.sync({ force: true }).then(() => console.log('PaymentMethod model synchronized.'));
+CardType.sync({ force: true }).then(() => console.log('CardType model synchronized.'));
+Card.sync({ force: true }).then(() => console.log('Card model synchronized.'));
+Bank.sync({ force: true }).then(() => console.log('Bank model synchronized.'));
+AddMethod.sync({ force: true }).then(() => console.log('AddMethod model synchronized.'));
 Skill.sync({ force: true }).then(() => console.log('Skill model synchronized.'));
 PlayerSkill.sync({ force: true }).then(() => console.log('PlayerSkill model synchronized.'));
 PlaygroundLocation.sync({ force: true }).then(() => console.log('PlaygroundLocation model synchronized.'));
