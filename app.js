96a64f13aff2f81ed6a74f414f3a119e0ec93be8
kimoabser <karimmosamaa22@gmail.com>
2024-09-24 12:08:04 -0400

    f2

--- app.js
+++ app.js
@@ -2,152 +2,58 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 require('dotenv').config();
+const User = require('./model/User');
+const Bank = require('./model/Bank');
+const Card = require('./model/Card');
+const CardType = require('./model/CardType');
+const AddMethod = require('./model/AddMethod');
+const Payment = require('./model/Payment');
+const PaymentMethod = require('./model/PaymentMethod');
+const Skill = require('./model/Skill');
+const PlayerSkill = require('./model/PlayerSkill');
+const PlaygroundLocation = require('./model/PlaygroundLocation');
+const PlaygroundPicture = require('./model/PlaygroundPicture');
+const Owner = require('./model/Owner');
+const GroundType = require('./model/GroundType');
+const City = require('./model/City');
+const Government = require('./model/Government');
+const BallType = require('./model/BallType');
+const Playground = require('./model/Playground');
+const Position = require('./model/Position');
+const PlayerPosition = require('./model/PlayerPosition');
+const PlayerLocation = require('./model/PlayerLocation');
+const UserFavoritePlayground = require('./model/UserFavoritePlayground');
+const Report = require('./model/Report');
+const Booking = require('./model/Booking');
+const CancellationReason = require('./model/CancellationReason');
+const JoinBooking = require('./model/JoinBooking');
 
-// Synchronize the model with the database
-User.sync({ alter: true }).then(() => {
-  console.log('User model synchronized with the database.');
-});
-
-// Synchronize the model with the database
-Bank.sync({ alter: true }).then(() => {
-  console.log('Bank model synchronized with the database.');
-});
-
-// Synchronize the model with the database
-Card.sync({ alter: true }).then(() => {
-  console.log('Card model synchronized with the database.');
-});
-
-// Synchronize the model with the database
-CardType.sync({ alter: true }).then(() => {
-  console.log('CardType model synchronized with the database.');
-});
-
-
-// Synchronize the model with the database
-AddMethod.sync({ alter: true }).then(() => {
-  console.log('AddMethod model synchronized with the database.');
-});
-
-
-// Synchronize the model with the database
-Payment.sync({ alter: true }).then(() => {
-  console.log('Payment model synchronized with the database.');
-});
-
-
-// Synchronize the model with the database
-PaymentMethod.sync({ alter: true }).then(() => {
-  console.log('PaymentMethod model synchronized with the database.');
-});
-
-// Synchronize the model with the database
-Skill.sync({ alter: true }).then(() => {
-  console.log('Skill model synchronized with the database.');
-});
-
-
-// Synchronize the model with the database
-PlayerSkill.sync({ alter: true }).then(() => {
-  console.log('PlayerSkill model synchronized with the database.');
-});
-
-
-// Synchronize the model with the database
-PlaygroundLocation.sync({ alter: true }).then(() => {
-  console.log('PlaygroundLocation model synchronized with the database.');
-});
-
-
-// Synchronize the model with the database
-PlaygroundPicture.sync({ alter: true }).then(() => {
-  console.log('PlayGroundPicture model synchronized with the database.');
-});
-
-// Synchronize the model with the database
-Owner.sync({ alter: true }).then(() => {
-  console.log('Owner model synchronized with the database.');
-});
-
-
-// Synchronize the model with the database
-GroundType.sync({ alter: true }).then(() => {
-  console.log('GroundType model synchronized with the database.');
-});
-
-// Synchronize the model with the database
-City.sync({ alter: true }).then(() => {
-  console.log('City model synchronized with the database.');
-});
-
-// Synchronize the model with the database
-Government.sync({ alter: true }).then(() => {
-  console.log('Government model synchronized with the database.');
-});
-
-
-// Synchronize the model with the database
-BallType.sync({ alter: true }).then(() => {
-  console.log('BallType model synchronized with the database.');
-});
-
-
-
-
-
-// Synchronize the model with the database
-Playground.sync({ alter: true }).then(() => {
-  console.log('PlayGround model synchronized with the database.');
-});
-
-
-
-// Synchronize the model with the database
-Position.sync({ alter: true }).then(() => {
-  console.log('Position model synchronized with the database.');
-});
-
-
-// Synchronize the model with the database
-PlayerPosition.sync({ alter: true }).then(() => {
-  console.log('PlayerPosition model synchronized with the database.');
-});
-
-
-
-// Synchronize the model with the database
-PlayerLocation.sync({ alter: true }).then(() => {
-  console.log('PlayerLocation model synchronized with the database.');
-});
-
-
-// Synchronize the model with the database
-UserFavoritePlayground.sync({ alter: true }).then(() => {
-  console.log('UserFavoritePlayground model synchronized with the database.');
-});
-
-
-
-// Synchronize the model with the database
-Report.sync({ alter: true }).then(() => {
-  console.log('Report model synchronized with the database.');
-});
-
-// Synchronize the model with the database
-Booking.sync({ force: false }).then(() => {
-  console.log('Booking model synchronized with the database.');
-});
-
-// Synchronize the model with the database
-CancellationReason.sync({ alter: true }).then(() => {
-  console.log('CancellationReason model synchronized with the database.');
-});
-
-
-JoinBooking.sync({ alter: true }).then(() => {
-  console.log('JoinBooking model synchronized with the database.');
-});
-
+// Synchronize model with the database
+User.sync({ alter: true }).then(() => console.log('User model synchronized.'));
+Bank.sync({ alter: true }).then(() => console.log('Bank model synchronized.'));
+Card.sync({ alter: true }).then(() => console.log('Card model synchronized.'));
+CardType.sync({ alter: true }).then(() => console.log('CardType model synchronized.'));
+AddMethod.sync({ alter: true }).then(() => console.log('AddMethod model synchronized.'));
+Payment.sync({ alter: true }).then(() => console.log('Payment model synchronized.'));
+PaymentMethod.sync({ alter: true }).then(() => console.log('PaymentMethod model synchronized.'));
+Skill.sync({ alter: true }).then(() => console.log('Skill model synchronized.'));
+PlayerSkill.sync({ alter: true }).then(() => console.log('PlayerSkill model synchronized.'));
+PlaygroundLocation.sync({ alter: true }).then(() => console.log('PlaygroundLocation model synchronized.'));
+PlaygroundPicture.sync({ alter: true }).then(() => console.log('PlaygroundPicture model synchronized.'));
+Owner.sync({ alter: true }).then(() => console.log('Owner model synchronized.'));
+GroundType.sync({ alter: true }).then(() => console.log('GroundType model synchronized.'));
+City.sync({ alter: true }).then(() => console.log('City model synchronized.'));
+Government.sync({ alter: true }).then(() => console.log('Government model synchronized.'));
+BallType.sync({ alter: true }).then(() => console.log('BallType model synchronized.'));
+Playground.sync({ alter: true }).then(() => console.log('Playground model synchronized.'));
+Position.sync({ alter: true }).then(() => console.log('Position model synchronized.'));
+PlayerPosition.sync({ alter: true }).then(() => console.log('PlayerPosition model synchronized.'));
+PlayerLocation.sync({ alter: true }).then(() => console.log('PlayerLocation model synchronized.'));
+UserFavoritePlayground.sync({ alter: true }).then(() => console.log('UserFavoritePlayground model synchronized.'));
+Report.sync({ alter: true }).then(() => console.log('Report model synchronized.'));
+Booking.sync({ alter: true }).then(() => console.log('Booking model synchronized.'));
+CancellationReason.sync({ alter: true }).then(() => console.log('CancellationReason model synchronized.'));
+JoinBooking.sync({ alter: true }).then(() => console.log('JoinBooking model synchronized.'));
 
 
 
