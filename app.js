56aa6373d246c3adc6ef296bc1ac601ba11a4f71
kimoabser <karimmosamaa22@gmail.com>
2024-09-24 12:58:13 -0400

    f12

--- app.js
+++ app.js
@@ -31,10 +31,10 @@ const Booking = require('./model/Booking');
 // Synchronize model with the database
 User.sync({ force: true }).then(() => console.log('User model synchronized.'));
 PaymentMethod.sync({ force: true }).then(() => console.log('PaymentMethod model synchronized.'));
-Card.sync({ force: true }).then(() => console.log('Card model synchronized.'));
-Payment.sync({ force: true }).then(() => console.log('Payment model synchronized.'));
 CardType.sync({ force: true }).then(() => console.log('CardType model synchronized.'));
 Bank.sync({ force: true }).then(() => console.log('Bank model synchronized.'));
+Card.sync({ force: true }).then(() => console.log('Card model synchronized.'));
+Payment.sync({ force: true }).then(() => console.log('Payment model synchronized.'));
 AddMethod.sync({ force: true }).then(() => console.log('AddMethod model synchronized.'));
 Skill.sync({ force: true }).then(() => console.log('Skill model synchronized.'));
 PlayerSkill.sync({ force: true }).then(() => console.log('PlayerSkill model synchronized.'));
